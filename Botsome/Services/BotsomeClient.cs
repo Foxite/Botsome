d6879ec722da34e6dd224554e490f28c54821cb3
Dirk Kok <the@dirkkok.nl>
2022-11-29 12:25:30 +0100

    fix response group

--- Botsome/Services/BotsomeClient.cs
+++ Botsome/Services/BotsomeClient.cs
@@ -118,6 +118,10 @@ public class BotsomeClient : IAsyncDisposable {
 		IEnumerable<string> requiredEmotes = botsomeItem.Responses
 			.Where(response => response.Type is ResponseType.EmoteNameAsMessage or ResponseType.EmoteNameAsReaction)
 			.Select(response => response.Response);
+
+		if (!string.IsNullOrWhiteSpace(botsomeItem.RespondGroup) && !Bot.Groups.Contains(botsomeItem.RespondGroup)) {
+			return false;
+		}
 				
 		return requiredEmotes.All(emote => m_EmotesByName.ContainsKey(emote));
 	}
