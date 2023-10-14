a9dfbdf423e0adf7a00bef5e648ccba611914987
Dirk Kok <the@dirkkok.nl>
2023-10-14 19:08:33 +0200

    [ci skip] add comment to clarify diagnostic feature

--- Botsome/Services/BotsomeClient.cs
+++ Botsome/Services/BotsomeClient.cs
@@ -45,6 +45,7 @@ public class BotsomeClient : IAsyncDisposable {
 		m_OnChangeListener = statusOptions.OnChange(newOptions => UpdateStatus(newOptions));
 
 		discord.MessageCreated += (_, ea) => {
+			// Note: this is a temporary diagnostic for issue #17
 			if (!ea.Channel.GuildId.HasValue && (ea.Guild != null || ea.Channel.Guild != null || ea.Channel.Type is not (ChannelType.Group or ChannelType.Private))) {
 				notificationService.SendNotification($"Channel object missing guildId when receiving message: {ea.Guild?.Id}/{ea.Channel?.Id}/{ea.Message?.Id} by {ea.Author?.Id} {ea.Message?.JumpLink}");
 			}
